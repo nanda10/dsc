c9866235f0b0f4b7bb9a016abded21740aaec0bb
Nanda <53907486+nanda10@users.noreply.github.com>
2019-10-07 20:55:52 +0530

    Add files via upload

---
+++ Int_str_float.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package int_str_float;
+
+/**
+ *
+ * @author Nandan R
+ */
+import java.util.*;
+public class Int_str_float {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String st=s.nextLine();
+        int n=s.nextInt();
+        double dou=s.nextDouble();
+        System.out.println("String value is "+st+"\n"+"Integer value is "+n+"\n"+"Double value is "+dou);
+    }
+    
+}
